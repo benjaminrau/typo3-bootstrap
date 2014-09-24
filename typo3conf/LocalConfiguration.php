f59879ee32d6fe7661d0e1718010ba9f26de9fd4
Benjamin Rau <rau@codearts.at>
2014-09-24 13:49:55 +0200

    Init typo3 bootstrap

---
+++ typo3conf/LocalConfiguration.php
@@ -0,0 +1,79 @@
+<?php
+return array(
+	'BE' => array(
+		'debug' => TRUE,
+		'explicitADmode' => 'explicitAllow',
+		'installToolPassword' => '',
+		'loginSecurityLevel' => 'rsa',
+		'versionNumberInFilename' => '0',
+	),
+	'DB' => array(
+		'database' => '',
+		'extTablesDefinitionScript' => 'extTables.php',
+		'host' => '127.0.0.1',
+		'password' => '',
+		'port' => 3306,
+		'socket' => '',
+		'username' => '',
+	),
+	'EXT' => array(
+		'extConf' => array(
+			'linkvalidator' => 'a:0:{}',
+			'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+			'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+			'scheduler' => 'a:5:{s:11:"maxLifetime";s:4:"1440";s:11:"enableBELog";s:1:"1";s:15:"showSampleTasks";s:1:"1";s:11:"useAtdaemon";s:1:"0";s:30:"listShowTaskDescriptionAsHover";s:1:"1";}',
+		),
+	),
+	'FE' => array(
+		'activateContentAdapter' => FALSE,
+		'contentRenderingTemplates' => array(
+			'fluidcontentcore/Configuration/TypoScript/',
+		),
+		'debug' => TRUE,
+		'loginSecurityLevel' => 'rsa',
+		'pageAccessRestricted_handling_statheader' => 'HTTP/1.0 403 Forbidden',
+		'pageNotFound_handling' => '',
+		'pageNotFound_handling_statheader' => 'HTTP/1.0 404 Not Found',
+	),
+	'GFX' => array(
+		'colorspace' => 'sRGB',
+		'im' => 1,
+		'im_mask_temp_ext_gif' => 1,
+		'im_noScaleUp' => '1',
+		'im_path' => '/usr/bin/',
+		'im_path_lzw' => '/usr/bin/',
+		'im_v5effects' => -1,
+		'im_version_5' => 'im6',
+		'image_processing' => 1,
+		'jpg_quality' => '80',
+	),
+	'SYS' => array(
+		'caching' => array(
+			'cacheConfigurations' => array(
+				'extbase_object' => array(
+					'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+					'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+					'groups' => array(
+						'system',
+					),
+					'options' => array(
+						'defaultLifetime' => 0,
+					),
+				),
+			),
+		),
+		'clearCacheSystem' => FALSE,
+		'compat_version' => '6.2',
+		'devIPmask' => '',
+		'displayErrors' => FALSE,
+		'enableDeprecationLog' => FALSE,
+		'encryptionKey' => '1e0c87d6151ef01d278156c179aa5f49576dc4e16bcf9894a6ba970740aa66a00781a25a7380049b624265ddbf3af3d8',
+		'isInitialInstallationInProgress' => FALSE,
+		'sitename' => 'TYPO3 Bootstrap',
+		'sqlDebug' => 0,
+		'systemLogLevel' => 2,
+		't3lib_cs_convMethod' => 'mbstring',
+		't3lib_cs_utils' => 'mbstring',
+	),
+);
+?>
